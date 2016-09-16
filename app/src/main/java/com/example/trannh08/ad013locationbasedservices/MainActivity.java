365f13259ebf30545096b172a13ceaaa253790f5
Koichi <koichisojiro@live.com>
2016-09-16 10:17:16 +0700

    initial

---
+++ app/src/main/java/com/example/trannh08/ad013locationbasedservices/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.trannh08.ad013locationbasedservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
